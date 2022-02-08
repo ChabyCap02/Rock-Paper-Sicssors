82b513256c06463262a86d8db46a6f3d4f2f03f3
FCPjunior <90715542+ChabyCap02@users.noreply.github.com>
2022-02-08 12:16:14 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Elementos PJ
+const containerRespuestaPj = document.querySelector(".opcion-pj");
+const containerPj = document.querySelectorAll(".container-opcion-j1");
+
+const opcion1 = containerPj.node;
+
+console.log(containerPj);
+
+// Elementos Bot
+
+
+// Boton Jugar
+const botonGame = document.querySelector(".boton-jugar");
+
+const jugar = () =>{
+    console.log("hola");
+};
+
+botonGame.addEventListener('click', jugar());
