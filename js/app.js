88b8b8f5b20574e22ca05eec76610f4e32ac9a53
FCPjunior <90715542+ChabyCap02@users.noreply.github.com>
2022-02-08 18:32:53 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //--- Elementos Start Game
 const opcionGame = document.querySelector(".container-start-buttons");
 const containerGame = document.querySelector(".container-game");
+const botonRegresar = document.querySelector('.container-return');
 
 const opcionGamePlayer = opcionGame.childNodes[1];
 const opcionGameTwoPlayers = opcionGame.childNodes[3];
@@ -11,17 +12,30 @@ opcionGamePlayer.addEventListener("click", ()=>{
     containerGame.style.visibility = "visible";
     document.querySelector(".container-start").style.visibility = "hidden";
     gameModeTwoPlayers = false;
+    botonRegresar.style.display = 'block';
+
 });
 opcionGameTwoPlayers.addEventListener('click', ()=>{
     containerGame.style.visibility = "visible";
     document.querySelector(".container-start").style.visibility = "hidden";
     gameModeTwoPlayers = true;
     if(gameModeTwoPlayers){
-        const hola = document.querySelector('.container-opcion-bot .card-element').classList.toggle('diseableM')
-        console.log(hola)
+        const hola = document.querySelector('.container-opcion-bot').childNodes
+        hola[1].classList.toggle('diseableM');
+        hola[3].classList.toggle('diseableM');
+        hola[5].classList.toggle('diseableM');
     }
+    botonRegresar.style.display = 'block';
 });
+botonRegresar.addEventListener('click', ()=>{
+    containerGame.style.visibility = 'hidden';
+    botonRegresar.style.display = 'none';
+})
+
+
 //Funcion para habilitar el Pj2
+
+//Boton Regresar
 
 
 //--- Elementos PJ
